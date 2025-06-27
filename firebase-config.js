9724d27df48efc8aeba0b3f6804d0e49f3f79a72
shikarinx <shikarin.xx@gmail.com>
2025-06-28 00:34:13 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAqtfmxxvVhbRBX-05O1AhSGmgxK8O8ncE",
+  authDomain: "monetra-66446.firebaseapp.com",
+  projectId: "monetra-66446",
+  storageBucket: "monetra-66446.firebasestorage.app",
+  messagingSenderId: "1081175709983",
+  appId: "1:1081175709983:web:7baadd2aec2e7dfeb5382a",
+  measurementId: "G-W1KN10SC4D"
+};
